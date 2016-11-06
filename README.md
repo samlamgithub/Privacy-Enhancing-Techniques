c52ae184fd299fa16490c93284a7d955f9dfb7db
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2016-11-06 11:36:45 +0000

    initialise cw

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Privacy-Enhancing-Techniques
 Privacy Enhancing Techniques CW
+
+
+To Run the program, ensure Python 2.7.6 is installed. And then simply Run
+```
+python answer.py
+```
---
+++ answer.py
@@ -0,0 +1,11 @@
+"""Coursework for 408H - Privacy Enhancing Techniques"""
+# -*- coding: utf-8 -*-
+import numpy as np
+import math
+
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
