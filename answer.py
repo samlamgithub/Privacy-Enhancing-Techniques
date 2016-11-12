710ba054567853ca88479cfbe5be9c3417ffbb63
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2016-11-12 14:31:08 +0000

    final

--- answer.py
+++ answer.py
@@ -1,65 +1,19 @@
-"""Coursework for 408H - Privacy Enhancing Techniques"""
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+"""Coursework for 408H - Privacy Enhancing Techniques"""
 import numpy as np
-import math
 import numpy.random as ran
-import cryption
+from Crypto import Random
 from Crypto.Cipher import AES
-
-
+import base64
 
 PADDING = '{'
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 unpad = lambda s: s.split(PADDING)[0]
-
-
-
-def Encrypt(k1, k2, k, x):
-    """
-    An Encrypt function, encrypting (k, x)
-    using the given keys k1 and k2 with AES.
-    Padding must be added to keys and message
-    because it's usually under 16 bytes required
-    by AES.
-    As x is 0 or 1, the message is the key k shifted
-    by one bit, and this last bit (LSB) is replaced by x.
-    """
-    # print "xxx", k1, k2, k, x, "xxx"
-    message = str(int(x) + (k << 1))
-    message = pad(str(message))
-
-    key1 = pad(str(k1))
-    key2 = pad(str(k2))
-
-    obj = AES.new(key1, AES.MODE_CBC, key2)
-
-    return obj.encrypt(message)
-
-
-def Decrypt2(k1, k2, ciphertext):
-    """
-    A Decrypt function, decrypting x and k
-    using the given keys k1 and k2.
-    """
-    # print "xxx2 xx", k1, k2, ciphertext, "xxx"
-    key1 = pad(str(k1))
-    key2 = pad(str(k2))
-
-    obj = AES.new(key1, AES.MODE_CBC, key2)
-
-    message = obj.decrypt(ciphertext)
-    message = unpad(str(message))
-    message = int(message)
-
-    k = message >> 1
-    x = message & 1
-
-    return k, x
-
 AliceInputBits = np.arange(2)
 BobInputBits = np.arange(2, 4)
+BS = 16
 
 def And(a, b):
     return bool(a) and bool(b)
@@ -76,36 +30,40 @@ def Nor(a, b):
 def Xor(a, b):
     return bool(a) != bool(b)
 
+def encrypt(key, raw):
+    raw = pad(raw)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return base64.urlsafe_b64encode(iv + cipher.encrypt(raw))
+
+def decrypt(key, enc):
+    enc = base64.urlsafe_b64decode(enc.encode('utf-8'))
+    iv = enc[:BS]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[BS:]))
+
 def Encry(kX, kY, kZ, t):
-    # message = str(int(t) + (kZ << 1))
-    # cipher = AES.new(cryption.pad(str(kX)), AES.MODE_CBC, cryption.pad(str(kY)))
-    # encoded = cryption.EncodeAES(cipher, message)
-    # return encoded
-    # print False
-    #xxx 125 196 250 0 xxx
-    return Encrypt(int(kX), int(kY), int(kZ), t)
+    message = pad(str(int(t) + (kZ << 1)))
+    key = pad(str(kX+kY))
+    return encrypt(key, message)
 
 def Decrypt(kX, kY, text):
-
-    # cipher = AES.new(cryption.pad(str(kX)), AES.MODE_CBC,cryption.pad(str(kY)))
-    # decoded = cryption.DecodeAES(cipher, text)
-    # decoded = RemovePadding(decoded)
-    # decoded = int(decoded)
-    # k = decoded >> 1
-    # x = decoded & 1
-    # return k, x
-    # # print "xxx", kX, kY, t, "xxx"
-    # xxx2 xx 13 146 luanma xxx
-    return Decrypt2(int(kX), int(kY), text)
+    key = pad(str(kX+kY))
+    message = int(decrypt(key, text))
+    t, kZ = message >> 1, message & 1
+    return t, kZ
 
 def PrintBit(B):
     if bool(B):
-        return "1"
+        return "1 "
     else:
-        return "0"
+        return "0 "
 
 def BinaryToDecimal(B):
     return 2 * int(B[0]) + int(B[1])
+
+def PrintDecimal(D):
+    return str(D/2) + str(D%2)
 
 def CountWire(circuit):
     if isinstance(circuit, list):
@@ -116,27 +74,25 @@ def CountWire(circuit):
 def CircuitApplyOperation(circuit, index, operation):
     if not isinstance(circuit, list):
         return [operation(circuit, index)]
-
     total = CountWire(circuit)
-    Returns = [[None, None]] * total
+    Returns = total * [np.array([None, None])]
     firstWireCount = CountWire(circuit[1])
     Returns[1:(firstWireCount+1)] = CircuitApplyOperation(circuit[1], index + 1, operation)
-    secondWireCount = CountWire(circuit[2])
-    Returns[(firstWireCount+1):] = CircuitApplyOperation(circuit[2], index + firstWireCount +1, operation)
+    Returns[(firstWireCount+1):] = CircuitApplyOperation(circuit[2], index + firstWireCount + 1, operation)
     return Returns
 
 def ConstructTableForGate(gate, kKey, pKey):
-    table = [['', ''], ['', '']]
+    table = []
     for i in np.arange(2):
+        js = []
         for j in np.arange(2):
             x = int(Xor(i, pKey[1]))
             y = int(Xor(j, pKey[2]))
             z = int(gate(x, y))
             t = int(Xor(z, pKey[0]))
-            tmp2 = Encry(kKey[1][x], kKey[2][y], kKey[0][z], t)
-            # print "tmp2: ", tmp2
-            table[i][j] = tmp2
-    # print table
+            cipher = Encry(kKey[1][x], kKey[2][y], kKey[0][z], t)
+            js.append(cipher)
+        table.append(js)
     return table
 
 def ConstructTablesForCircuit(circuit, kKey, pKey):
@@ -144,60 +100,53 @@ def ConstructTablesForCircuit(circuit, kKey, pKey):
         return [None]
     totoalWireCount = CountWire(circuit)
     FirstWireCount = CountWire(circuit[1])
-    # print "FirstWireCount: ", FirstWireCount
     FirstGateCount = FirstWireCount / 2
-    Tables = [None] * (totoalWireCount/2)
+    GarbedTables = (totoalWireCount/2) * [None]
     Gate = circuit[0]
-    Tables[1:(FirstGateCount+1)] = ConstructTablesForCircuit(circuit[1], kKey[1:(FirstWireCount+1), :], pKey[1:(FirstWireCount+1)])
-    Tables[(FirstGateCount+1):] = ConstructTablesForCircuit(circuit[2], kKey[(FirstWireCount+1):, :], pKey[(FirstWireCount+1):])
+    GarbedTables[1:(FirstGateCount+1)] = ConstructTablesForCircuit(circuit[1], kKey[1:(FirstWireCount+1), :], pKey[1:(FirstWireCount+1)])
+    GarbedTables[(FirstGateCount+1):] = ConstructTablesForCircuit(circuit[2], kKey[(FirstWireCount+1):, :], pKey[(FirstWireCount+1):])
     kKeyForCurrentGate = [kKey[0, :], kKey[1, :], kKey[FirstWireCount+1, :]]
     pKeyForCurrentGate = [pKey[0], pKey[1], pKey[FirstWireCount+1]]
-    Tables[0] = ConstructTableForGate(Gate, kKeyForCurrentGate, pKeyForCurrentGate)
-    # print "x ", x
-    # print "table: ", Tables
-    return Tables
+    GarbedTables[0] = ConstructTableForGate(Gate, kKeyForCurrentGate, pKeyForCurrentGate)
+    return GarbedTables
 
 def ConstructGarbledCircuit(circuit, InputBits):
     TotalWireCount = CountWire(circuit)
-    kKeys = ran.random_integers(0, 256, [TotalWireCount, 2])
+    kKeys = ran.random_integers(0, 1024, [TotalWireCount, 2])
     pKeys = ran.random_integers(0, 1, [TotalWireCount])
     GarbleTables = ConstructTablesForCircuit(circuit, kKeys, pKeys)
-
     AliceEncyInput = CircuitApplyOperation(circuit, 0, lambda Input, Index: Xor(InputBits[int(Input)], int(pKeys[Index])) if int(Input) in AliceInputBits else None)
     AliceKKeys = CircuitApplyOperation(circuit, 0, lambda Input, Index: kKeys[Index, int(InputBits[int(Input)])] if int(Input) in AliceInputBits else None)
-
     BobPKeys = CircuitApplyOperation(circuit, 0, lambda Input, Index: pKeys[Index] if int(Input) in BobInputBits else None)
     BobPKeys[0] = pKeys[0]
-    BobKKeys = CircuitApplyOperation(circuit, 0, lambda Input, Index: kKeys[Index, :] if int(Input) in BobInputBits else [0, 0])
+    BobKKeys = CircuitApplyOperation(circuit, 0, lambda Input, Index: kKeys[Index, :] if int(Input) in BobInputBits else np.array([0, 0]))
     BobKKeys[0] = kKeys[0, :]
-    BobKKeys = np.matrix(BobKKeys)
-
-    return [circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys, BobKKeys]
+    BobKKeys = np.matrix(BobKKeys).reshape([len(BobKKeys), 2])
+    return circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys, BobKKeys
 
 def GetCircuitForIndexInputWire(circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys, BobKKeys, Index):
-    Input1WireCount = CountWire(circuit[1])
-    Input1GateCount = Input1WireCount / 2
-    IndexWireCount = CountWire(circuit[Index])
-    IndexGateCount = IndexWireCount / 2
+    FirstWireCount = CountWire(circuit[1])
+    FirstWireGateCount = FirstWireCount / 2
 
-    WIdxMin = (Index-1)*Input1WireCount + 1
-    WIdxMax = WIdxMin + IndexWireCount
-    GIdxMin = (Index-1)*Input1GateCount + 1
-    GIdxMax = GIdxMin + IndexGateCount
+    if Index == 1:
+        return circuit[1], GarbleTables[1:1+FirstWireGateCount],AliceEncyInput[1:1+FirstWireCount], AliceKKeys[1:1+FirstWireCount], BobPKeys[1:1+FirstWireCount],BobKKeys[1:1+FirstWireCount, :]
+    elif Index == 2:
+        SecondWireCount = CountWire(circuit[2])
+        SecondWireGateCount = SecondWireCount / 2
+        SecondWireStartIndex = FirstWireCount + 1
+        SecondWireEndIndex = SecondWireStartIndex + SecondWireCount
+        SecondWireGateStartIndex = FirstWireGateCount + 1
+        SecondWireGateEndIndex = SecondWireGateStartIndex + SecondWireGateCount
 
-    return  circuit[Index], GarbleTables[GIdxMin:GIdxMax],AliceEncyInput[WIdxMin:WIdxMax], AliceKKeys[WIdxMin:WIdxMax], BobPKeys[WIdxMin:WIdxMax],BobKKeys[WIdxMin:WIdxMax, :]
+        return circuit[2], GarbleTables[SecondWireGateStartIndex:SecondWireGateEndIndex],AliceEncyInput[SecondWireStartIndex:SecondWireEndIndex], AliceKKeys[SecondWireStartIndex:SecondWireEndIndex], BobPKeys[SecondWireStartIndex:SecondWireEndIndex], BobKKeys[SecondWireStartIndex:SecondWireEndIndex, :]
 
 def EvaluateCircuit(circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys, BobKKeys, Input):
-    # print "input: ", Input
-    # print "BobKKeys; ", BobKKeys
     if not isinstance(circuit, list):
         if circuit in AliceInputBits:
             kKey, EncryedValue = AliceKKeys[0], AliceEncyInput[0]
         else:
-            BobInputBit = circuit - 2
-            # print "BobInputBit: ", BobInputBit
-            tmp = Input[BobInputBit]
-            kKey = BobKKeys[0, tmp]
+            BobInputBit = int(circuit - 2)
+            kKey = BobKKeys[0, Input[BobInputBit]]
             EncryedValue = int(Xor(BobPKeys[0], Input[BobInputBit]))
         return kKey, EncryedValue
 
@@ -206,75 +155,36 @@ def EvaluateCircuit(circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys,
     circuit2, GarbleTables2, AliceEncyInput2, AliceKKeys2, BobPKeys2, BobKKeys2 = GetCircuitForIndexInputWire(circuit, GarbleTables, AliceEncyInput, AliceKKeys, BobPKeys, BobKKeys, 2)
     kKey2, EncryedValue2 = EvaluateCircuit(circuit2, GarbleTables2, AliceEncyInput2, AliceKKeys2, BobPKeys2, BobKKeys2, Input)
     GateTable = GarbleTables[0]
-    EncryedOutput = GateTable[EncryedValue1][EncryedValue2]
-    # print "xxx", kKey1, kKey2, EncryedOutput, "xxx"
+    EncryedOutput = GateTable[int(EncryedValue1)][int(EncryedValue2)]
     kZ, t = Decrypt(kKey1, kKey2, EncryedOutput)
     return kZ, t
 
-def cmp(a, b):
-    if a==b:
+def cmp(a, b, c, d):
+    Alice = BinaryToDecimal([a, b])
+    Bob = BinaryToDecimal([c, d])
+    if Alice == Bob:
         return 0
-    if a>b:
+    if Alice > Bob:
         return 1
-    if a<b:
+    if Alice < Bob:
         return 3
-    return 999
 
 def main(circuit):
-    # print "Truth-Table for function '%s':\n" % (FuncName)
-    # print "   AB | CD || EF"
-    # print "  ----|----||----"
-    # p = pad("vjwoiowsivjwsadaa")
-    # print p
-    # x = unpad(p)
-    # print x
-    # return
+    print "A ", "B ", "C ", "D ", ":  ", "O1  ", "O2  ", "CMP"
     for A in [False, True]:
         for B in [False, True]:
             AliceInput = (A, B)
-            AliceInputInt = BinaryToDecimal(AliceInput)
             circuitOut1, GarbleTables1, AliceEncyInput1, AliceKKeys1, BobPKeys1, BobKKeys1 = ConstructGarbledCircuit(circuit[0], AliceInput)
             circuitOut2, GarbleTables2, AliceEncyInput2, AliceKKeys2, BobPKeys2, BobKKeys2 = ConstructGarbledCircuit(circuit[1], AliceInput)
-            # print "BobKKeys1: " , BobKKeys1
-            # print "BobKKeys2: " , BobKKeys2
             Out1DecryKey = BobPKeys1[0]
             Out2DecryKey = BobPKeys2[0]
-
             for C in [False, True]:
                 for D in [False, True]:
-                    # Evaluate each garbled circuit with the second input
                     _, EncryedOut1 = EvaluateCircuit(circuitOut1, GarbleTables1, AliceEncyInput1, AliceKKeys1, BobPKeys1, BobKKeys1, (C, D))
                     _, EncryedOut2 = EvaluateCircuit(circuitOut2, GarbleTables2, AliceEncyInput2, AliceKKeys2, BobPKeys2, BobKKeys2, (C, D))
                     Out1 = Xor(EncryedOut1, Out1DecryKey)
                     Out2 = Xor(EncryedOut2, Out2DecryKey)
-
-                    BobInputInt = BinaryToDecimal((C, D))
-                    OutInt = BinaryToDecimal((Out1, Out2))
-                    # print OutInt
-                    # Print the result line
-                    print "   %d%d | %d%d || %d%d    (%d, %d) = %d !!! %d" % \
-                        (int(A), int(B), int(C), int(D), int(Out1), int(Out2), #FuncName,
-                         AliceInputInt, BobInputInt, OutInt, cmp(AliceInputInt, BobInputInt))
-
-
-                    #
-                    # if FuncValid(AliceInputInt, BobInputInt) != OutInt:
-                    #     print "There's something wrong..."
-                    #     sys.exit(1)
-    # Print a new-line at the end
-    # print ''
-    # print len(circuit)
-    # O1 = circuit[0]
-    # print Compute2(O1)
-    # print 0, 1, 2, 3, ":  ", "O1", "O2"
-    # for A in [False, True]:
-    #         for B in [False, True]:
-    #                 for C in [False, True]:
-    #                         for D in [False, True]:
-    #                             print Compute2(O1, A, B, C, D)
-                                # O1, O2 = Compute(A, B, C, D)
-                                # print PrintBit(A), PrintBit(B), PrintBit(C), PrintBit(D), ":  ", PrintBit(O1), " ", PrintBit(O2)
-
+                    print PrintBit(A), PrintBit(B), PrintBit(C), PrintBit(D), ":  ", PrintBit(Out1), " ", PrintBit(Out2), " ", PrintDecimal(cmp(A, B, C, D))
     return 0
 
 if __name__ == "__main__":
